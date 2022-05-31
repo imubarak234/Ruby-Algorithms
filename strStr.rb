809c2270219b7a8cee6da8a678c78f3e4cf42789
Mubarak <imubarak2424@gmail.com>
2022-05-31 11:07:30 +0100

    progress on strStr

---
+++ strStr.rb
@@ -0,0 +1,9 @@
+def strStr(haystack, needle)
+  finalAnswer = -1
+  length = needle.length - 1
+
+  if needle.length == 0{
+    finalAnswer = 0;
+  }
+  elsif haystack.length >= needle.haystack
+end
