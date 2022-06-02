6ed516e95c40803fde6611eded6d14e54e7b9832
Mubarak <imubarak2424@gmail.com>
2022-06-02 10:52:25 +0100

    complete strstr algorithm

--- strStr.rb
+++ strStr.rb
@@ -6,30 +6,28 @@ def strStr(haystack, needle)
   if needle.length == 0
     finalAnswer = 0
   elsif haystack.length >= needle.length
-    haystack.split('').each_with_index {
-      |n, idx|
+    haystack.split('').each_with_index do |n, idx|
       pointerX = idx
       count = 0
       if needle[0] == haystack[idx]
-        needle.split('').each_with_index {
-          |y, idy|
+        needle.split('').each_with_index do |y, idy|
           if needle[idy] == haystack[pointerX]
-            count++
+            count += 1
           end
-          pointerX++
-        }
+          pointerX += 1
+        end
       end
       if count == needle.length
         finalAnswer = idx
         break
       end
-    }
+    end
   end
 
   finalAnswer
 end
 
-strStr('hello','ll')
+puts strStr('hello','ll')
 
 var = 'hello'
 puts var
--- test.rb
+++ test.rb
@@ -4,4 +4,8 @@ var.split('').each_with_index do |n, idx|
   puts n
   puts idx
 end
+
+var_one = 8
+var_one += 1
+puts var_one
 # puts var.split('')
