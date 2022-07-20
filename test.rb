0187fc162bf66f67fe56bb2ebfbccfa79cb79672
Mubarak <imubarak2424@gmail.com>
2022-07-20 19:36:16 +0100

    new

--- test.rb
+++ test.rb
@@ -5,7 +5,14 @@ var.split('').each_with_index do |n, idx|
   puts idx
 end
 
-var_one = 8
-var_one += 1
-puts var_one
+# var_one = 8
+# var_one += 1
+# puts var_one
+
+def output(number)
+  var = ["Premium", "Standard", "Delux", "Executive", "StakeHolder"]
+  var[rand(0..4)]
+end
+
+puts output(2)
 # puts var.split('')
